4a8b5ea7e18cf4d0f63871dc7fafc0546db2491e
이민우 <dlehfud22@naver.com>
2020-04-01 15:36:34 +0900

    [20200401 이민우] mysql 연결

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,8 +82,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "test",
+        "USER": "root",
+        "PASSWORD": "ssafy",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "OPTIONS": {"init_command": 'SET sql_mode="STRICT_TRANS_TABLES"'},
     }
 }
 
