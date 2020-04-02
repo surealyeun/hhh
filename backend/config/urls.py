5efee66eeba7b1f7c94658a91d88aebbfd8f556e
Eumyounghyun <opwer032@naver.com>
2020-04-02 13:51:12 +0900

    [200402 음영현] redoc 수정

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,28 +13,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
-from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
-from rest_framework import routers
+from django.urls import path, include
+from django.conf.urls import url
 from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
 from drf_yasg import openapi
-
-import users.views
-
-router = routers.DefaultRouter()
-router.register("users", users.views.UserViewSet)
-
-# prefix = movies , viewset = MovieViewSet
+from .yasg import schema_view, schema_url_patterns
 
 urlpatterns = [
-    url(r"^admin/", admin.site.urls),
-    url(r"^", include(router.urls)),
-    url(r"^api/doc", get_swagger_view(title="Rest API Document")),
+    path("admin/", admin.site.urls),
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
