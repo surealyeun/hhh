a3c6d78a424892f16f2d1dcb1f410b00ea5c12ce
이민우 <dlehfud22@naver.com>
2020-04-07 13:04:25 +0900

    S02P22A202-25 [200407 이민우] place, keywords commit

--- backend/places/admin.py
+++ backend/places/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Store)
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Location)
+class LocationAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    pass
