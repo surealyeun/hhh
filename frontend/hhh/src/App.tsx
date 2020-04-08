665be7a16f794a6b9ac755529863fbae8d7eb2e8
문요한 <munyohan@naver.com>
2020-04-08 10:44:20 +0900

    [20200408 문요한] FeedList, Feed 컴포넌트 생성

--- frontend/hhh/src/App.tsx
+++ frontend/hhh/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import FeedList from './components/feedList/FeedList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>ㅎㅎㅎ</h1>
+      <FeedList></FeedList>
     </div>
   );
 }
