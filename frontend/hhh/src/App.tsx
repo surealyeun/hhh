2cbeaa90c7e4c3d33480da33171a3efd57a98e75
surealyeun <monkyw@naver.com>
2020-04-09 15:52:57 +0900

    [20200409 한수연] main안에 html파일 렌더링, 디자인 수정

--- frontend/hhh/src/App.tsx
+++ frontend/hhh/src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 import Main from './components/main/Main';
-import ScrollCard from "./components/hotPlace/ScrollCard";
-import FeedList from './components/feedList/FeedList';
+// import ScrollCard from "./components/hotPlace/ScrollCard";
+// import FeedList from './components/feedList/FeedList';
 
 function App() {
   return (
