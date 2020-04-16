f3b170763fd03b2f41526d836aab377667e7dd69
surealyeun <monkyw@naver.com>
2020-04-16 17:03:01 +0900

    [20200416 한수연] 장소 상세페이지

--- frontend/hhh/src/components/main/Main.tsx
+++ frontend/hhh/src/components/main/Main.tsx
@@ -1,28 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Main.scss";
-
-// import seoul from "../image/seoul_grey.png";
-
-// var map = './mapmini.html';
-// var template = {__html: __html};
 
 class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                {/* <div className="circle1"></div> */}
-                {/* <div className="circle2"></div>
-                <div className="circle3"></div> */}
+                <div className="test">
+                    <h1>TEST</h1>
+                        <h4>클릭하면 해당 페이지로 넘어갑니다.</h4>
+                        <Link to="/login">
+                            1. 유저 로그인 페이지
+                        </Link><br/>
+                        <Link to="/userInfo">
+                            2. 회원 정보 수정 페이지
+                        </Link><br/>
+                        <Link to="/spotlist">
+                            3. 장소 리스트 페이지
+                        </Link><br/>
+                        <Link to="/place">
+                            4. 장소 상세 페이지
+                        </Link>
+                </div>
                 <div className="grid">
                     <div className="title">
-                        <h1>ㅎ<br/>ㅎ<br/>ㅎ</h1>
+                        <h1>
+                            ㅎ<br />ㅎ<br />ㅎ
+                        </h1>
                         <h4>
-                            <span id="h4-1">핫,</span><br/>
-                            <span id="h4-2">힙,</span><br/>
-                            <span id="h4-3">힐링,</span><br/>
+                            <span id="h4-1">핫,</span>
+                            <br />
+                            <span id="h4-2">힙,</span>
+                            <br />
+                            <span id="h4-3">힐링,</span>
+                            <br />
                             <span id="h4-4">플레이스</span>
                         </h4>
-                        
+                        <hr />
                     </div>
                     <div className="mapFrame">
                         <iframe className="map" src="../map.html" frameBorder="0" />
