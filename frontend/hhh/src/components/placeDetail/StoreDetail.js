4df3e28e7d897b781748889c6856fd268ea79666
heeseonim <hisundl@naver.com>
2020-04-20 22:06:32 +0900

    [20200420 임희선] 사진추가

--- frontend/hhh/src/components/placeDetail/StoreDetail.js
+++ frontend/hhh/src/components/placeDetail/StoreDetail.js
@@ -1,5 +1,6 @@
 /*global kakao*/
 import React from "react";
+import { Row, Col } from "antd";
 import "./PlaceDetail.scss";
 
 class PlaceDetail extends React.Component {
@@ -23,7 +24,10 @@ class PlaceDetail extends React.Component {
         const map = new window.kakao.maps.Map(container, options);
 
         // 마커가 표시될 위치입니다
-        var markerPosition = new kakao.maps.LatLng(this.props.location.state.latitude, this.props.location.state.longitude);
+        var markerPosition = new kakao.maps.LatLng(
+          this.props.location.state.latitude,
+          this.props.location.state.longitude
+        );
 
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
@@ -43,39 +47,82 @@ class PlaceDetail extends React.Component {
     console.log(this.props);
     return (
       <div className="place-detail">
-        <h1 className="place-name">{store_name}</h1>
-        <div>
-          <p>{category}</p>
-        </div>
-        <hr />
-        <div className="description">
-          <h3>Description</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <br /> <hr />
-        <div className="location">
-          <h3>Location</h3>
-          <div className="map" id="Mymap"></div>
-        </div>
-        <br /> <hr />
-        <div className="reviews">
-          <h3>Reviews</h3>
-          <div className="review"></div>
-          <div className="review"></div>
-          <div className="review"></div>
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
+        <Row>
+          <Col span={11}>
+            <Row gutter={16}>
+              <Col span={24}>
+                <div className="empty" />
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-atl3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/92028574_152110496140923_1238325184368526124_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com&_nc_cat=103&_nc_ohc=LUKxaXWCmbcAX8-aYm4&oh=977f61027864985c24962340097d8d67&oe=5EAF8F10" />
+              </Col>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-atl3-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/91523822_505695946973748_4618136914309432735_n.jpg?_nc_ht=scontent-atl3-1.cdninstagram.com&_nc_cat=100&_nc_ohc=cPwyIMYbdNYAX9KR5r2&oh=fb055704713c3bac2169e00bc0240288&oe=5EB23135" />
+              </Col>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/92462223_1528148800680967_6557145994758711521_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=107&_nc_ohc=JdQcb32YsjQAX-cVqAp&se=7&oh=6e0fed0ff2eee3ce4082ea3443aea6ca&oe=5EC8448A&ig_cache_key=MjI4MjA5MzUwOTY0NTE1MjI4OQ%3D%3D.2" />
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <div className="empty2" />
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/92210321_222417495485454_8828049053151549869_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=106&_nc_ohc=CnP67USwd1IAX81Nxu2&se=7&oh=3898693d80c33942fa0f210514761fa2&oe=5EC8B1B2&ig_cache_key=MjI4MjA4ODcxNDU1NjE2NjgxNQ%3D%3D.2" />
+              </Col>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/91978608_134913158084830_2765144029097195256_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=110&_nc_ohc=ty1JqScPtV4AX93Ofmi&se=7&oh=0001d19bae32e84886ae40e8cbac128e&oe=5EC68058&ig_cache_key=MjI4MjA3OTU0MDY2NTAyODkxOQ%3D%3D.2" />
+              </Col>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-lga3-1.cdninstagram.com/v/t51.2885-15/e35/92566770_152891716088094_2844173153369313983_n.jpg?_nc_ht=scontent-lga3-1.cdninstagram.com&_nc_cat=106&_nc_ohc=ZZkRYv-1cbIAX-m9SUQ&se=7&oh=53999d87196f8279985b3466189798e1&oe=5EC64B2E&ig_cache_key=MjI4MjA3NDk4Mzc0NzYwMzIxOA%3D%3D.2" />
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col span={24}>
+                <div className="empty2" />
+              </Col>
+            </Row>
+            <Row gutter={16}>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/92245979_831655963994693_5110124155454270836_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=DdXoCoMV2PQAX9yZK1p&se=7&oh=90a8bd1df431c150c3a022c304bf9f96&oe=5EC655DE&ig_cache_key=MjI4MjAwNjAwNDIwNjEyNTUzMQ%3D%3D.2" />
+              </Col>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/92259511_123040675996035_1076069689526094587_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=103&_nc_ohc=V_ANpOZv2UcAX_7ExX5&se=8&oh=058670cfeebdec5a0873d64bd509c55d&oe=5EC5BF2D&ig_cache_key=MjI4MTgyNTU0NzcwMDMwMDc3OA%3D%3D.2" />
+              </Col>
+              <Col className="gutter-row" span={8}>
+                <img src="https://scontent-yyz1-1.cdninstagram.com/v/t51.2885-15/e35/91909496_533547517347244_5752511906769169757_n.jpg?_nc_ht=scontent-yyz1-1.cdninstagram.com&_nc_cat=104&_nc_ohc=5i9ZJYgXnB4AX-90hWa&oh=0adf839cb81b23040ed4e09b8f832e0c&oe=5EC83B40&ig_cache_key=MjI4MTgzNjkzMTMzMzcyNjIzNg%3D%3D.2" />
+              </Col>
+            </Row>
+          </Col>
+          <Col span={1} />
+          <Col span={12}>
+            <h1 className="place-name">{store_name}</h1>
+            <div>
+              <p>{category}</p>
+            </div>
+            <hr />
+            <div className="location">
+              <h3>Location</h3>
+              <div className="map" id="Mymap"></div>
+            </div>
+            <br />
+            <hr />
+            <div className="reviews">
+              <h3>Reviews</h3>
+              <div className="review"></div>
+              <div className="review"></div>
+              <div className="review"></div>
+            </div>
+            <br /> <hr />
+            <br />
+            <br />
+            <br />
+          </Col>
+        </Row>
       </div>
     );
   }
