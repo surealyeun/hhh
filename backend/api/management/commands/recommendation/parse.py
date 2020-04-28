e170f53429f9769c164745ae43a98892b31d6334
Eumyounghyun <opwer032@naver.com>
2020-04-28 13:55:59 +0900

    [20200428 음영현] HHH 맞춤 유저 빅데이터 추천 init

--- backend/api/management/commands/recommendation/parse.py
+++ backend/api/management/commands/recommendation/parse.py
@@ -4,6 +4,7 @@ import numpy as np
 import os
 import shutil
 import datetime
+import pymysql
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.decomposition import TruncatedSVD
 from scipy.sparse.linalg import svds
@@ -12,93 +13,70 @@ DATA_DIR = "./data/"
 DATA_FILE = os.path.join(DATA_DIR, "dining.json")
 DUMP_FILE = os.path.join(DATA_DIR, "dining.pkl")
 
-def import_data(data_path=DATA_FILE):
+def import_data():
     
-    data = load_dataframes()
-    df_review = data["reviews"]
-    df_store = data["stores"]
+    connection = pymysql.connect(
+        host='13.125.113.171', user='root', password='ssafya202!@#', db='HHH',
+        charset='utf8mb4', autocommit=True, cursorclass=pymysql.cursors.DictCursor
+    )
 
-    df = pd.merge(df_review, df_store, left_on='store', right_on='id', how='outer')
-    df.rename(columns={'id_x':'id'}, inplace=True)
-    """
-        # 협업 필터링 알고리즘 - 아이템 기반 추천
-    
-    store_user_rating = df.pivot_table('score', index = 'id_y', columns='user')
-    store_user_rating.fillna(0, inplace=True)
+    cursor = connection.cursor()
+    sql = "select * from HHH.api_diningstore"
+    cursor.execute(sql)
 
-    item_based_collabor = cosine_similarity(store_user_rating)
+    result = cursor.fetchall()
+    df_store = pd.DataFrame(result)
 
-    item_based_collabor = pd.DataFrame(data = item_based_collabor, index=store_user_rating.index, columns=store_user_rating.index)
-    print(item_based_collabor)
+    sql = "select * from HHH.api_location"
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    df_location = pd.DataFrame(result)
 
-    def get_item_based_collabor(title):
-        return item_based_collabor[title].sort_values(ascending=False)
-    
-    print(get_item_based_collabor(360348))
-    
-    """
+    connection.close()
 
-    """
-        # 협업 필터링 알고리즘 - 유저 기반 추천
+    print(df_store)
+    print(df_location)
 
-    store_user_rating = df.pivot_table('score', index = 'user', columns='id_y')
-    store_user_rating.fillna(0, inplace=True)
-    print(store_user_rating)
-    user_based_collabor = cosine_similarity(store_user_rating)
+    # df = pd.merge(df_review, df_store, left_on='store', right_on='id', how='outer')
+    # df.rename(columns={'id_x':'id'}, inplace=True)
 
-    user_based_collabor = pd.DataFrame(data = user_based_collabor, index=store_user_rating.index, columns=store_user_rating.index)
-
-    print(user_based_collabor)
-
-    def get_user_based_collabor(title):
-        return user_based_collabor[title].sort_values(ascending=False)
-    
-    print(get_user_based_collabor(115682))
-    """
-    """
-    Matrix SVD 추천
-    SVD = TruncatedSVD(n_components=12)
-    matrix = SVD.fit_transform(user_store_rating)
-
-    corr = np.corrcoef(matrix)
-
-    store_title = user_dining_store_rating.columns
-    store_title_list = list(store_title)
-    coffey_hands = store_title_list.index('03수제비칼국수')
-    print(coffey_hands)
-    corr_coffey_hands = corr[coffey_hands]
-    print(list(store_title[(corr_coffey_hands >= 0.9)])[:50])
-    """
     """
         유저 맞춤 가게 추천
     """
-    address_list = []
-    df = df[df['address_gu'] == '서초구']
+    gu_list = [
+        '서초구','동작구','관악구','금천구','영등포구','구로구','양천구','강서구',
+        '강남구','송파구','강동구','광진구','중랑구','노원구','성동구','동대문구',
+        '성북구','강북구','도봉구','종로구','중구','용산구','마포구','서대문구',
+        '은평구'
+    ]
+    # for i in range(1):#len(gu_list)):
+    #     address_list = []
+    #     df = df[df['address_gu'] == gu_list[i]]
 
-    # gu 기준으로 위에서 정렬해서 넘어 와야함
-    df.drop(['id','branch', 'area', 'category', 'address_see', 'address_gu', 'address_dong'],axis='columns', inplace=True)
-    df['user'] = df['user'].fillna(0).astype(int)
-    df['score'] = df['score'].fillna(0).astype(int)
-    df['store'] = df['store'].fillna(0).astype(int)
+    #     # gu 기준으로 위에서 정렬해서 넘어 와야함
+    #     df.drop(['id','branch', 'area', 'category', 'address_see', 'address_gu', 'address_dong'],axis='columns', inplace=True)
+    #     df['user'] = df['user'].fillna(0).astype(int)
+    #     df['score'] = df['score'].fillna(0).astype(int)
+    #     df['store'] = df['store'].fillna(0).astype(int)
 
-    user_dining_store_rating = df.pivot_table('score', index='user', columns='store').fillna(0)
-    user_num_list = list(user_dining_store_rating.index)
-    
-    matrix = user_dining_store_rating.as_matrix()
-    user_ratings_mean = np.mean(matrix, axis=1)
-    matrix_user_mean = matrix - user_ratings_mean.reshape(-1, 1)
-    df = pd.DataFrame(matrix_user_mean, columns=user_dining_store_rating.columns)
-    U, sigma, Vt = svds(matrix_user_mean, k = 12)
-    sigma = np.diag(sigma)
+    #     user_dining_store_rating = df.pivot_table('score', index='user', columns='store').fillna(0)
+    #     user_num_list = list(user_dining_store_rating.index)
+        
+    #     matrix = user_dining_store_rating.as_matrix()
+    #     user_ratings_mean = np.mean(matrix, axis=1)
+    #     matrix_user_mean = matrix - user_ratings_mean.reshape(-1, 1)
+    #     df = pd.DataFrame(matrix_user_mean, columns=user_dining_store_rating.columns)
+    #     U, sigma, Vt = svds(matrix_user_mean, k = 12)
+    #     sigma = np.diag(sigma)
 
-    svd_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
-    df_svd_preds = pd.DataFrame(svd_user_predicted_ratings, columns=user_dining_store_rating.columns)
-    
-    usernumber_index = user_num_list.index(13)
-    already_rated, predictions = recommend_spot(df_svd_preds, usernumber_index, df_store, df_review, 10, user_num_list)
+    #     svd_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
+    #     df_svd_preds = pd.DataFrame(svd_user_predicted_ratings, columns=user_dining_store_rating.columns)
+        
+    #     usernumber_index = user_num_list.index(13)
+    #     already_rated, predictions = recommend_spot(df_svd_preds, usernumber_index, df_store, df_review, 10, user_num_list)
 
-    print(already_rated)
-    print(predictions)
+    #     print(already_rated)
+    #     print(predictions)
 
 
 def recommend_spot(df_svd_preds, user, ori_spot_df, ori_ratings_df, num_recommendations=20, user_list = []):
