50f3578a1a789c5e89dd264fcbd8bd44a9fed526
이민우 <dlehfud22@naver.com>
2020-04-29 15:54:12 +0900

    [20200429 이민우] login

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -57,6 +57,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
     url(
-        r"^redoc/$", schema_view.with_ui("redoc", czache_timeout=0), name="schema-redoc"
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
     ),
 ] + user_urlpatterns + api_urlpatterns
